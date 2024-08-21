8e809cd180ffe3923af1576bd93d0f6bf77dc4b0
sarthak4399 <sarthakkhandare21@gmail.com>
2024-08-21 20:55:12 +0530

    chore: Update Tailwind CSS and PostCSS dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
+  darkMode: 'class',
   content: [
-
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        light: {
-          bg: "#ffffff",
-          text: "#343a40",
-          sidebarBg: "#e9ecef",
-          topbarBg: "#ced4da",
-          border: "#adb5bd",
-        },
-        dark: {
-          bg:"#111111",
-          text: "#f8f9fa",
-          sidebarBg: "#101113",
-          topbarBg: "#495057",
-          border: "#343a40",
-
-        },
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  
-  plugins: [],
-};
+  plugins: [
+    function ({ addUtilities }) {
+        const newUtilities = {
+            ".no-scrollbar::-webkit-scrollbar": {
+                display: "none",
+            },
+            ".no-scrollbar": {
+                "-ms-overflow-style": "none",
+                "scrollbar-width": "none",
+            },
+        };
+        addUtilities(newUtilities);
+    },
+],
+}
+
